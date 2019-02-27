2b5c2f52d625d4dc4e21baea2b2575fe9ecbc97e
Linus Oleander <linus@oleander.nu>
2019-02-28 04:00:53 +0800

    ok

--- Sources/Logger/ListItem.swift
+++ Sources/Logger/ListItem.swift
@@ -8,8 +8,19 @@ public struct ListItem {
     return key.count
   }
 
+  private var strValue: String {
+    if let bool = value as? Bool {
+      return bool ? "yes".lightGreen.dim : "no".lightRed.dim
+    }
+
+    return String(describing: value)
+  }
+
+  private func paddedKey(_ indent: Int) -> String {
+    return key.padding(toLength: indent, withPad: " ", startingAt: 0)
+  }
+
   public func formatted(indent: Int) -> String {
-    let paddedStr = key.padding(toLength: indent, withPad: " ", startingAt: 0)
-    return paddedStr + ": " + String(describing: value)
+    return paddedKey(indent) + ": " + strValue
   }
 }
